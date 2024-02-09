1e7095dace091a1f1cf8e798aa5749e6a21971f5
Aniket Nandi <122383360+7marksinclair@users.noreply.github.com>
2024-02-09 11:11:58 +0530

    Update app.py

--- app.py
+++ app.py
@@ -68,7 +68,7 @@ data_training_array = scaler.fit_transform(data_training)
 
 #Loading model
 
-model = load_model('keras_model.h5')
+model = load_model('keras_model.h5', custom_objects={'CustomAdam': CustomAdam})
 
 #Testing part
 
